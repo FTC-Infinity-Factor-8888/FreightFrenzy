58bd435cce22c0b3e37ebeb1239d8a1497a599d4
Pranai Rao <pranai@rao.me>
2021-11-14 13:33:47 -0500

    Deleted all files
    I deleted all the files so we can get the new TO program into Java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoal/EmergencyStopException.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class EmergencyStopException extends RuntimeException {
-    public EmergencyStopException(String message) {
-        super(message);
-    }
-}
