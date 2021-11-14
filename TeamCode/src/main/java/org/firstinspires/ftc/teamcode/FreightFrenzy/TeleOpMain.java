d10914d1d9bb61aee687cc0f95e25fe6f1cf61f4
Helen Saunders <hsaunders@hollismontessori.org>
2021-11-14 14:27:30 -0500

    BlocksToJava TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/TeleOpMain.java
@@ -0,0 +1,84 @@
+
+package org.firstinspires.ftc.teamcode.FreightFrenzy;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "TeleOpMain")
+public class TeleOpMain extends LinearOpMode {
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        double rSpeed;
+        double lSpeed;
+        double maxSpeed = 0.80;
+        double normalSpeed = 0.50;
+
+        DcMotor RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        DcMotor RRMotor = hardwareMap.get(DcMotor.class, "RRMotor");
+        DcMotor LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        DcMotor LRMotor = hardwareMap.get(DcMotor.class, "LRMotor");
+
+        // Put initialization blocks here.
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RRMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                // Put loop blocks here.
+                if (gamepad1.right_trigger >= 0.5) {
+                    rSpeed = gamepad1.right_stick_y * maxSpeed;
+                    lSpeed = gamepad1.left_stick_y * maxSpeed;
+                }
+                else {
+                    rSpeed = gamepad1.right_stick_y * normalSpeed;
+                    lSpeed = gamepad1.left_stick_y * normalSpeed;
+                }
+
+                if (gamepad1.right_bumper) {
+                    if (gamepad1.right_trigger >= 0.5) {
+                        LFMotor.setPower(-1 * maxSpeed);
+                        LRMotor.setPower(1 * maxSpeed);
+                        RFMotor.setPower(1 * maxSpeed);
+                        RRMotor.setPower(-1 * maxSpeed);
+                    }
+                    else {
+                        LFMotor.setPower(-1 * normalSpeed);
+                        LRMotor.setPower(1 * normalSpeed);
+                        RFMotor.setPower(1 * normalSpeed);
+                        RRMotor.setPower(-1 * normalSpeed);
+                    }
+                }
+                else if (gamepad1.left_bumper) {
+                    if (gamepad1.right_trigger >= 0.5) {
+                        LFMotor.setPower(1 * maxSpeed);
+                        LRMotor.setPower(-1 * maxSpeed);
+                        RFMotor.setPower(-1 * maxSpeed);
+                        RRMotor.setPower(1 * maxSpeed);
+                    }
+                    else {
+                        LFMotor.setPower(1 * normalSpeed);
+                        LRMotor.setPower(-1 * normalSpeed);
+                        RFMotor.setPower(-1 * normalSpeed);
+                        RRMotor.setPower(1 * normalSpeed);
+                    }
+                }
+                else {
+                    LFMotor.setPower(lSpeed);
+                    LRMotor.setPower(lSpeed);
+                    RFMotor.setPower(rSpeed);
+                    RRMotor.setPower(rSpeed);
+                }
+                telemetry.addData("Left stick", lSpeed);
+                telemetry.addData("Right stick", rSpeed);
+                telemetry.update();
+            }
+        }
+    }
+}
