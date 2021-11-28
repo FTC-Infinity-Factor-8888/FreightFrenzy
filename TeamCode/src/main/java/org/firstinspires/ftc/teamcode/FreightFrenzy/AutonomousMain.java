4fbe8afab7c0e49cb618d7bb585a92c0eaa01ec1
Helen Saunders <hsaunders@hollismontessori.org>
2021-11-28 15:07:20 -0500

    Added interface runs and added stop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/AutonomousMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/AutonomousMain.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.FreightFrenzy;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import org.firstinspires.ftc.teamcode.Utilities.iRobot;
 
-public class AutonomousMain {
-    public void supercalifragilisticexpialidocious() {
-        iRobot robot = new FreightFrenzyRobot();
-        robot.drive(0, 0, 0);
+public class AutonomousMain extends LinearOpMode {
+    public void food() {
+        iRobot robot = new FreightFrenzyRobot(this);
+        robot.drive(0,0);
+        robot.rotate(0);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/FreightFrenzyRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/FreightFrenzyRobot.java
@@ -1,14 +1,55 @@
 package org.firstinspires.ftc.teamcode.FreightFrenzy;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Utilities.iRobot;
 
 public class FreightFrenzyRobot implements iRobot {
+    private final HardwareMap hardwareMap;
+    DcMotor RFMotor;
+    DcMotor RRMotor;
+    DcMotor LFMotor;
+    DcMotor LRMotor;
+
+    public FreightFrenzyRobot (LinearOpMode creator){
+        this.hardwareMap = creator.hardwareMap;
+    }
+
+    private void initHardware () {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        RRMotor = hardwareMap.get(DcMotor.class, "RRMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        LRMotor = hardwareMap.get(DcMotor.class, "LRMotor");
+    }
+
+    @Override
+    public void drive(double y, double x) {
+
+    }
 
 
     @Override
-    public void drive(double y, double x, double r) {
+    public void rotate(double degrees) {
 
+    }
+
+    @Override
+    public void driveXYR(double y, double x, double r) {
+
+    }
+
+    @Override
+    public void driveStop() {
+        RFMotor.setPower(0.0);
+        RRMotor.setPower(0.0);
+        LFMotor.setPower(0.0);
+        LRMotor.setPower(0.0);
+    }
+
+    @Override
+    public void stopAll() {
+        driveStop();
     }
 }
