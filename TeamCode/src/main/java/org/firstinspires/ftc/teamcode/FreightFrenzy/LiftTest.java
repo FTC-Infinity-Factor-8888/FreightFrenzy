a538332615b89e8e229cd87c7ba63b2bf7eb336f
Pranai Rao <pranai@rao.me>
2021-12-01 19:24:09 -0500

    Lift Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/LiftTest.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "LiftTest (Blocks to Java)")
+public class LiftTest extends LinearOpMode {
+
+  private DcMotor LFMotor;
+
+  /**
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+  @Override
+  public void runOpMode() {
+    int minLiftPosition;
+    int maxLiftPosition;
+    double liftSpeed;
+
+    LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+
+    // Put initialization blocks here.
+    LFMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    waitForStart();
+    if (opModeIsActive()) {
+      // Put run blocks here.
+      while (opModeIsActive()) {
+        minLiftPosition = 0;
+        maxLiftPosition = 923;
+        liftSpeed = 0.2;
+        // Put loop blocks here.
+        telemetry.addData("Version: ", "v.3.1.0");
+        if (gamepad1.right_bumper) {
+          while (LFMotor.getCurrentPosition() < maxLiftPosition) {
+            if (LFMotor.getCurrentPosition() < maxLiftPosition) {
+              LFMotor.setPower(liftSpeed);
+              if (gamepad1.right_bumper == false) {
+                break;
+              }
+            }
+            if (gamepad1.b) {
+              while (opModeIsActive()) {
+                LFMotor.setPower(0);
+                LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                telemetry.addData("Status", " EMERGENCY BRAKE IS ACTIVE");
+                telemetry.addData("ERROR", "Please restart the OpMode");
+                telemetry.update();
+              }
+            }
+          }
+        } else if (gamepad1.left_bumper) {
+          while (LFMotor.getCurrentPosition() > minLiftPosition) {
+            if (LFMotor.getCurrentPosition() > minLiftPosition) {
+              LFMotor.setPower(-liftSpeed);
+              if (gamepad1.left_bumper == false) {
+                break;
+              }
+            } else {
+              break;
+            }
+            if (gamepad1.b) {
+              while (opModeIsActive()) {
+                LFMotor.setPower(0);
+                LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                telemetry.addData("Status", " EMERGENCY BRAKE IS ACTIVE");
+                telemetry.addData("ERROR", "Please restart the OpMode");
+                telemetry.update();
+              }
+            }
+          }
+        } else {
+          LFMotor.setPower(0);
+        }
+        telemetry.addData("Lift Position", LFMotor.getCurrentPosition());
+        telemetry.update();
+      }
+    }
+  }
+}
