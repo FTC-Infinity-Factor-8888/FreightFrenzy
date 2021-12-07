3fb7d01a2bb8c4ef8705b0987d7bc75cea8a0449
Helen Saunders <hsaunders@hollismontessori.org>
2021-12-07 17:26:25 -0500

    Changed TeleOp functions, began interface merge to FreightFrenzyRobot
    Added SithLordTeleOp as a test function.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/SithLordTeleOp.java
@@ -0,0 +1,14 @@
+
+package org.firstinspires.ftc.teamcode.FreightFrenzy;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "SithLordTeleOp")
+public class SithLordTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+}
