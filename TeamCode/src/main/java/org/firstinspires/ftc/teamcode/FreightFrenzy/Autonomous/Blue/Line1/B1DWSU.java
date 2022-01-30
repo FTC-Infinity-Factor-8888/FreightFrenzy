59613c3c1971a24399f96351a11d160906f20e45
Pranai Rao <pranai@rao.me>
2022-01-30 08:58:47 -0500

    Fixed conflicting opMode names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Blue/Line1/B1DWSU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Blue/Line1/B1DWSU.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.FreightFrenzy.FreightFrenzyRobot;
 
-@Autonomous (name = "R1DWSU")
+@Autonomous (name = "B1DWSU")
 public class B1DWSU extends LinearOpMode {
         FreightFrenzyRobot ewok;
         //start line 1 is the closest to the warehouse for both alliances (lined up with middle barcode)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Blue/Line1/B1WH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Blue/Line1/B1WH.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.FreightFrenzy.FreightFrenzyRobot;
 
-@Autonomous (name = "R1WH")
+@Autonomous (name = "B1WH")
 public class B1WH extends LinearOpMode {
     FreightFrenzyRobot ewok;
     //start line 1 is the closest to the warehouse for both alliances (lined up with middle barcode)
