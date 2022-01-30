0aff3185b2226e2c73a29431599d79198d306c06
Pranai Rao <pranai@rao.me>
2022-01-29 20:11:14 -0500

    Separate programs for warehouse line 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Red/Line1/R1WH.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.FreightFrenzy.Autonomous.Red.Line1;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.FreightFrenzy.FreightFrenzyRobot;
+
+@Autonomous (name = "R1WH")
+public class R1WH extends LinearOpMode {
+    FreightFrenzyRobot ewok;
+    //start line 1 is the closest to the warehouse for both alliances (lined up with middle barcode)
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //we have landed on a moon of Endor.
+        ewok = new FreightFrenzyRobot(this);
+        ewok.initHardware();
+
+        waitForStart();
+        if(opModeIsActive()){
+            ewok.drive(27);
+        }
+    }
+}
