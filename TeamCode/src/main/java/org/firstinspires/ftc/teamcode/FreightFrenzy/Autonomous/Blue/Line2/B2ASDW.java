1484befc7d560fadd69b3e91ef111a84f6ce928f
Pranai Rao <pranai@rao.me>
2022-02-01 17:27:33 -0500

    Added all autonomous files for Helen

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Blue/Line2/B2ASDW.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.FreightFrenzy.Autonomous.Blue.Line2;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.FreightFrenzy.FreightFrenzyRobot;
+
+
+@Autonomous(name = "B2ASDW")
+public class B2ASDW extends LinearOpMode {
+    FreightFrenzyRobot ewok;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //we have landed on a moon of Endor.
+        ewok = new FreightFrenzyRobot(this);
+        ewok.initHardware();
+        System.out.println("Bucket moved to DRIVE Position");
+
+        waitForStart();
+        if (opModeIsActive()) {
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/Autonomous/Red/Line1/R1ASWH.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.FreightFrenzy.Autonomous.Red.Line1;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.FreightFrenzy.FreightFrenzyRobot;
+
+
+@Autonomous(name = "R1ASWH")
+public class R1ASWH extends LinearOpMode {
+    FreightFrenzyRobot ewok;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //we have landed on a moon of Endor.
+        ewok = new FreightFrenzyRobot(this);
+        ewok.initHardware();
+        System.out.println("Bucket moved to DRIVE Position");
+
+        waitForStart();
+        if (opModeIsActive()) {
+
+        }
+    }
+}
